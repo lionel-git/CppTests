c1250f34ceffc20c5e92a5a3c42804261810fdaf
Lionel Desorme <lionel.desorme@free.fr>
2026-04-11 10:13:36 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+extern void test_map_wrapper();
+
+
+int main(int argc, char **argv)
+{
+    test_map_wrapper();
+    return 0;
+}
---
+++ test_map_wrapper.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+class MapWrapper
+{
+
+
+};
+
+void test_map_wrapper()
+{
+    std::cout << "Testing map wrapper..." << std::endl;
+    // This is a placeholder for the actual test code for the map wrapper.
+    // You can implement your test cases here to verify the functionality of the map wrapper.
+}
